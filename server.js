6b2a30f6314a9c03cdb894077fe1384bdbc8c953
MaartentePoele <maarten.te.poele@hva.nl>
2026-06-05 15:28:49 +0200

    #8 Post deels werkend gemaakt en dynamische steden toegevoegd

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set("views", "./views");
 
 app.get("/", async function (req, res) {
   const params = {};
-  
+
   const quickscanResponse = await fetch(
     "https://fdnd-agency.directus.app/items/ctc_smartzone?" +
       new URLSearchParams(params),
@@ -36,10 +36,40 @@ app.get("/quickscan", async function (req, res) {
   res.render("form.liquid");
 });
 
+app.post("/quickscan-post", async function (req, res) {
+  await fetch("https://fdnd-agency.directus.app/items/ctc_smartzone", {
+    method: "POST",
+    body: JSON.stringify({
+      comment: req.body.comment,
+      adress: req.body.adress,
+      // picture: req.body.picture,
+      city: req.body.city,
+      length: req.body.length,
+      // time: req.body.time,
+      monitoring_suitability: req.body.monitoring_suitability,
+      status: req.body.status,
+      // long: req.body.long,
+      // lat: req.body.lat,
+      smartzone_suitability: req.body.smartzone_suitability,
+      traffic_sign: req.body.traffic_sign,
+    }),
+
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  });
+
+  res.redirect(303, "/");
+});
+
 app.get("/:city", async function (req, res) {
   const params = {};
 
-  res.render("city.liquid");
+  const currentPath = req.path.replace(/^\//, "");
+
+  res.render("city.liquid", {
+    currentPath: currentPath,
+  });
 });
 
 app.get("/:city/:adress", async function (req, res) {
