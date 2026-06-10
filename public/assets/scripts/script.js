5d2423770501ea3ea6f3303ff0d32c1e6cd6b883
MaartentePoele <maarten.te.poele@hva.nl>
2026-06-10 11:07:24 +0200

    #12 loading en success state toegevoegd aan de submit button

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,6 +1,32 @@
 const menuButton = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 
+const submitButton = document.querySelector(".submit-button");
+const quickscanForm = document.querySelector(".quickscan-form");
+
+quickscanForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  submitButton.classList.add("loading");
+  submitButton.textContent = "loading...";
+
+  let formData = new FormData(quickscanForm);
+
+  const response = await fetch(quickscanForm.action, {
+    method: quickscanForm.method,
+    body: new URLSearchParams(formData),
+  });
+
+  submitButton.classList.remove("loading");
+  submitButton.classList.add("success");
+  submitButton.textContent = "Added!";
+
+  setTimeout(() => {
+    submitButton.classList.remove("success");
+    submitButton.textContent = "Submit";
+  }, 3000);
+});
+
 menuButton.addEventListener("click", function () {
   menuNav.classList.toggle("clicked");
   menuButton.classList.toggle("clicked");
